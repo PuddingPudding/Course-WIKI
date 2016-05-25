466899e90bd2135ba348c78d297f269879734974
PuddingPudding <hwsh70119@gmail.com>
2016-05-25 19:40:23 +0800

    首頁部分修改EditText與Button的id

--- app/src/main/java/com/example/acer/course_wiki/HomePage.java
+++ app/src/main/java/com/example/acer/course_wiki/HomePage.java
@@ -1,8 +1,6 @@
 package com.example.acer.course_wiki;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -25,7 +23,7 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        logInBtn = (Button)findViewById(R.id.logInBtn);
+        logInBtn = (Button)findViewById(R.id.BT_LogIn);
         logInBtn.setOnClickListener(goToMainPage);
     }
 
